8a23f644d7ec74ffc67d393acaf3347451bea76f
Andrew Hooker <geekoncoffee@gmail.com>
2014-02-14 10:20:54 -0600

    v6 - step 1, delete all the things

--- mandrill_endpoint.rb
+++ mandrill_endpoint.rb
@@ -5,21 +5,6 @@ class MandrillEndpoint < EndpointBase
 
   set :logging, true
 
-  post '/order_confirmation' do
-    order = OrderConfirmation.new(@message[:payload], @message[:message_id], @config)
-    process_result *order.consume
-  end
-
-  post '/order_cancellation' do
-    order = OrderCancellation.new(@message[:payload], @message[:message_id], @config)
-    process_result *order.consume
-  end
-
-  post '/shipment_confirmation' do
-    order = ShipmentConfirmation.new(@message[:payload], @message[:message_id], @config)
-    process_result *order.consume
-  end
-
   post '/send_mail' do
     # convert variables into Mandrill array / hash format.
     #
